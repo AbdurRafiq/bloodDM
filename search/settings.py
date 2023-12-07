e66ae0d4042153b8781cdd8fb3f63b3ec826c395
Abdur <abdur_rafiq@hotmail.com>
2023-12-07 08:27:40 -0700

    Commit

--- search/settings.py
+++ search/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '984o2!jl=j(a)h*g#ymj)hi@yh36rr$*5boy29+qxev=5%_!6!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
 
 
 # Application definition
